72189cd4c490dec76463397d3a68419619974853
Felipe Matheus <felipematheusdev@gmail.com>
2024-02-01 19:36:26 -0300

    feat: add name on token

--- src/controllers/user/UserController.js
+++ src/controllers/user/UserController.js
@@ -23,7 +23,7 @@ async function login(req, res) {
         const validPassword = await bcrypt.compare(req.body.password, user.password);
         if (!validPassword) return res.status(400).json({ message: 'Senha inválida.' });
 
-        const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+        const token = jwt.sign({ _id: user._id, name: user.name }, process.env.TOKEN_SECRET);
         res.status(201);
         res.header('auth-token', token).json({ token: token })
     } catch (error) {
