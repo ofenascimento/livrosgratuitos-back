ff03ab9458c031c6d93a63827bbd2cc54b3f91cb
gomestzx <felipematheusdev@gmail.com>
2024-03-15 05:53:48 -0300

    feat: add send support email function and route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 const LivrosRouter = require('./routes/livros');
 const UserRouter = require('./routes/users');
+const sendSupportEmail = require('./utils/sendSupportEmail');
 
 const app = express();
 connectDB();
@@ -14,7 +15,18 @@ app.use(express.json());
 app.use('/livros', LivrosRouter);
 app.use('/users', UserRouter);
 
+app.post('/send-email', (req, res) => {
+    const { userEmail, userName, subject, message } = req.body;
+    try {
+        sendSupportEmail(userEmail, userName, subject, message )
+        res.status(200).json({message: 'Email enviado com sucesso!'})
+    } catch(error) {
+        res.status(500).json({ message: error.message });
+    }
+    
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('\x1b[32m',`Servidor iniciado na porta ${port}`, '\x1b[0m');
+    console.log('\x1b[32m', `Servidor iniciado na porta ${port}`, '\x1b[0m');
 });
---
+++ src/utils/sendSupportEmail.js
@@ -0,0 +1,44 @@
+const nodemailer = require('nodemailer');
+
+async function sendSupportEmail(userEmail, userName, subject, message) {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD,
+        },
+    });
+
+    let mailOptions = {
+        from: process.env.EMAIL_FROM,
+        to: process.env.EMAIL_SUPPORT,
+        subject: `Suporte LV: ${subject}`,
+        html: `
+        <table width="100%" bgcolor="#F1F5F8" border="0" cellpadding="0" cellspacing="0">
+    <tr>
+        <td>
+            <table align="center" bgcolor="#fff" style="width: 600px; margin: 0 auto; border-collapse: collapse; padding: 40px; border: 10px solid #F1F5F8;">
+                <tr>
+                    <td style="text-align: center; padding: 20px;">
+                        <h1 style="color: #333;">${subject}</h1>
+                        <p style="color: #000; font-size: 16px;">email ${userName}</p>
+                        <p style="color: #000; font-size: 16px;">email ${userEmail}</p>
+                        <p style="color: #000;">${message}</p>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+        `,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log('E-mail de suporte enviado com sucesso!');
+    } catch (error) {
+        console.log('Erro ao enviar e-mail de suporte:', error);
+    }
+}
+
+module.exports = sendSupportEmail;
