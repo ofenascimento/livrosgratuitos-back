048c5aead0b7cadfac318ad801c29806a8dc13fb
ofenascimento <felipematheusdev@gmail.com>
2026-07-23 15:06:10 -0300

    refactor: split app.js from server.js and fix package.json entrypoint

---
+++ src/app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const app = require('./app');
+const connectDB = require('../config/database');
+
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOSTNAME || 'localhost';
+
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log('\x1b[32m', `Servidor iniciado na porta ${port}`, '\x1b[0m');
+    console.log('\x1b[34m', `API rodando em: http://${hostname}:${port}`, '\x1b[0m');
+  });
+});
