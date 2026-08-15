7bccf85b1bd4dcb1614a18aacf015e7ca25b3254
ofenascimento <felipematheusdev@gmail.com>
2026-08-15 00:10:37 -0300

    fix: use req.user._id instead of req.user.id in getEpubReadingList

--- src/modules/reading-progress/controllers/readingProgress.controller.js
+++ src/modules/reading-progress/controllers/readingProgress.controller.js
@@ -2,16 +2,16 @@ const catchAsync = require('../../../utils/catchAsync');
 const readingProgressService = require('../services/readingProgress.service');
 
 exports.saveProgress = catchAsync(async (req, res) => {
-  const progress = await readingProgressService.saveProgress(req.user.id, req.body);
+  const progress = await readingProgressService.saveProgress(req.user._id, req.body);
   res.json(progress);
 });
 
 exports.getProgress = catchAsync(async (req, res) => {
-  const progress = await readingProgressService.getProgress(req.user.id, req.params.bookId);
+  const progress = await readingProgressService.getProgress(req.user._id, req.params.bookId);
   res.json(progress || null);
 });
 
 exports.getEpubReadingList = catchAsync(async (req, res) => {
-  const epubReadingList = await readingProgressService.getEpubReadingList(req.user.id);
+  const epubReadingList = await readingProgressService.getEpubReadingList(req.user._id);
   res.json(epubReadingList);
 });
