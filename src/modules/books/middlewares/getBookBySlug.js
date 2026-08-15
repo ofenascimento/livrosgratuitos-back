5e58325073b069079a35d3ca41453b8239e68d49
ofenascimento <felipematheusdev@gmail.com>
2026-08-14 23:38:53 -0300

    fix: correct getBookBySlug middleware setting res.livro instead of res.book

--- src/modules/books/middlewares/getBookBySlug.js
+++ src/modules/books/middlewares/getBookBySlug.js
@@ -1,14 +1,14 @@
-const Livro = require('../../../models/Book');
+const Book = require('../../../models/Book');
 
 async function getBookBySlug(req, res, next) {
     try {
-        const livro = await Livro.findOne({ slug: req.params.slug });
+        const book = await Book.findOne({ slug: req.params.slug });
 
-        if (!livro) {
+        if (!book) {
             return res.status(404).json({ message: 'Não foi possível encontrar o livro' });
         }
 
-        res.livro = livro;
+        res.book = book;
         next();
     } catch (err) {
         return res.status(500).json({ message: err.message });
